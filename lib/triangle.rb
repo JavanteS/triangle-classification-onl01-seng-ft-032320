4efa1cbc633f8a2db2402f5e5183944cd2951632
Javante Nelson <javantenelson@gmail.com>
2020-04-09 00:24:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,6 +16,10 @@ class Triangle
    elsif @a + @b < @c || @a + @c < @b || @b + @c < @a
      raise TriangleError
    else 
+     if @a == @b && @b == @c && @a == @c
+       :equilateral 
+     elsif 
+        @a 
      
    end
  end 
